010c242b4376cb07b3c62966834755cd40bee22f
Jackson Emmerich <jacksonemmerich@gmail.com>
2022-08-09 23:46:27 -0400

    first commit, and insert client in h2 db

---
+++ src/main/java/com/jacksonemmerich/trabfinalcap1/Trabfinalcap1Application.java
@@ -0,0 +1,13 @@
+package com.jacksonemmerich.trabfinalcap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabfinalcap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabfinalcap1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacksonemmerich/trabfinalcap1/Trabfinalcap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacksonemmerich.trabfinalcap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabfinalcap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
